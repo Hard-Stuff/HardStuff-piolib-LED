d49c240a77e3d6ee26ad7bb00dee306cf203e400
Matt-Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2024-03-25 09:24:37 +0000

    improved readme and platformio registry save

--- examples/main.cpp
+++ examples/main.cpp
@@ -7,17 +7,24 @@
 
 void setup()
 {
-    LED::init(0x999922);
+    // Turn to ~purprle
+    LED::init(0x992299);
 
     delay(1000);
+
+    // Turn off
     LED::toggleLED();
     delay(1000);
+
+    // Turn to red
     LED::setLEDRGB(0xff0000);
+    // Immediately turn off (because setLEDRGB turns it on)
     LED::toggleLED();
 }
 
 void loop()
 {
     delay(1000);
+    // Toggle forever
     LED::toggleLED();
 }
--- src/Hard-Stuff-Onboard-LED.hpp
+++ src/Hard-Stuff-Onboard-LED.hpp
@@ -12,6 +12,11 @@ namespace LED
     CRGB leds[1];
     static CRGB lastColour = CRGB(255, 255, 255); // Initialize with a default colour
 
+    /**
+     * @brief Set the LED to a given colour
+     *
+     * @param colour best set via 0xRRGGBB
+     */
     void setLEDRGB(uint32_t colour)
     {
         int R = (colour >> 8) & 0xFF;  // Extract the red component
@@ -22,15 +27,23 @@ namespace LED
         FastLED.show();
     }
 
-    bool init(uint32_t start_colour = 0xffffff)
+    /**
+     * @brief Initialise the onboard LED
+     *
+     * @param start_colour defaults to white, override with 0xRRGGBB
+     */
+    void init(uint32_t start_colour = 0xffffff)
     {
         pinMode(LED_GPIO, OUTPUT);
         digitalWrite(LED_GPIO, LOW);
         FastLED.addLeds<NEOPIXEL, LED_GPIO>(leds, 1); // GRB ordering is assumed
         setLEDRGB(start_colour);
-        return true;
     }
 
+    /**
+     * @brief Toggle the LED On/Off
+     *
+     */
     void toggleLED()
     {
         leds[0] = (leds[0] == lastColour) ? CRGB::Black : lastColour;
